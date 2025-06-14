596127b826afb974cf80e34c28f4011887559c48
Sanzhar <omarkhanovsanzh@gmail.com>
2025-06-15 02:27:14 +0500

    Added useLocation to track the click

--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -1,6 +1,7 @@
 //chatlist.tsx
 import ChatItem from './ChatItem';
 import React from 'react';
+import { useNavigate } from 'react-router-dom'
 
 interface ChatData {
   userName: string;
@@ -13,21 +14,31 @@ interface ChatListProps {
 }
 
 const ChatList: React.FC<ChatListProps> = ({ chats }) => {
+
+  const navigate = useNavigate()
+  const handleClick = (value:string) => {
+  navigate('/chat', { state: { value } })
+  }
+  
   return (
     <div className='flex flex-col w-[300px] h-full bg-white'>
       <div className='h-15 flex items-center bg-white border-b p-3'>
         <h1 className='text-2xl font-black'>Chats</h1>
       </div>
 
-      <div className='flex-1 overflow-y-auto'>
-        {chats.map((chat, index) => (
+      <div className='overflow-y-auto'>
+      {chats.map((chat) => (
+        <button
+          onClick={() => handleClick(chat.userName)}
+          className="w-full"
+        >
           <ChatItem
-            key={index}
             userName={chat.userName}
             lastMsg={chat.lastMsg}
             lastMsgTime={chat.lastMsgTime}
           />
-        ))}
+        </button>
+      ))}
       </div>
     </div>
   );
--- src/components/ChatRoom.tsx
+++ src/components/ChatRoom.tsx
@@ -1,18 +1,50 @@
 //chatroom.tsx
-import { useState } from 'react';
-import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
-import SendRoundedIcon from '@mui/icons-material/SendRounded';
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
 import BasicMenu from "./BasicMenu"
 import Message from './Message';
 
-function ChatRoom() {
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import SendRoundedIcon from '@mui/icons-material/SendRounded';
+
+
+interface ChatData {
+  userName: string;
+  lastMsgTime: string;
+  lastMsg: string;
+}
+
+interface ChatRoomProps {
+  data: ChatData[];
+}
+
+const ChatRoom: React.FC<ChatRoomProps> = ({ data }) => {
   const [msg, setMsg] = useState('')
+  const location = useLocation();
+  const { value } = location.state || {};
+  
+  
+  let message : string = "Select a chat to start messaging";
+  let name:string = "";
+  let time:string = "";
+
+
+  data.map((chat)=>{
+    if (chat.userName === value) {
+      message = chat.lastMsg
+      name = chat.userName
+      time = chat.lastMsgTime
+    }
+  })
+
+
   return (
-    <div className="flex flex-col flex-1 h-full bg-yellow-300">
+    <div className="flex flex-col flex-1 h-full bg-blue-200">
       <div className="bg-white flex justify-between h-16 items-center px-3">
         <div className="flex items-center gap-2">
           <img src="../../avatar.png" alt="userAvatar" className='w-10 h-10 rounded-full object-cover' />
-          <h1>UserName</h1>
+          <h1>{name}</h1>
         </div>
         <div className="flex items-center ">
           <SearchRoundedIcon
@@ -30,8 +62,17 @@ function ChatRoom() {
 
       
       <div className='flex flex-col h-full max-h-screen'>
-        <div className='flex-1 overflow-y-auto p-4'>
-          <Message />
+        <div className='flex flex-col-reverse items-end flex-1 overflow-y-auto p-4'>
+          <div className='border-1 bg-white rounded-lg '>
+            <Message
+            userName={name}
+            lastMsg={message}
+            lastMsgTime={time}
+            />
+          </div>
+
+
+
         </div>
 
         <div className='p-4 border-t bg-white flex gap-2'>
