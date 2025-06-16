304a1ee7c7cf5c0f54856688f28b57377d7b07a0
Sanzhar <omarkhanovsanzh@gmail.com>
2025-06-17 01:18:21 +0500

    Fixed bug with infinite loading

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,15 +1,13 @@
 //chat.tsx
 import ChatList from '../components/ChatList';
-import ChatRoom from '../components/ChatRoom';
 
 
 export default function Chat() {
+
   return (
     <div className='flex h-full'>
       <ChatList />
-      <ChatRoom />
     </div>
-    
-    
   );
+  
 }
